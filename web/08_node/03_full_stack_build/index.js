d4772088f1886c68b53bd70d177bf24a2dca801f
andydo365 <huconghuan@126.com>
2023-07-03 16:44:56 +0800

    add localStore for node 3document search

--- web/08_node/03_full_stack_build/index.js
+++ web/08_node/03_full_stack_build/index.js
@@ -1,47 +1,31 @@
-const express = require('express');
+const express = require('express'); //引入express框架
+
+// 引入node方法
 const path = require('path');
+const fs = require('fs/promises');
 
+const app = express(); //创建express实例
 
-const app = express();
-
-let students = [
-    {
-        id: 1,
-        name: 'andy',
-        age: 18,
-        gender: 'male',
-
-    },
-    {
-        id: 2,
-        name: 'bob',
-        age: 28,
-        gender: 'male',
-
-    },
-    {
-        id: 3,
-        name: 'red',
-        age: 38,
-        gender: 'male',
-
-    }
-]
+// 把data文件中的数据用students接收
+let students = require('./data/students.json')
 
 app.set('view engine', 'ejs') //设置模块
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views')) // 第一个是node中的名字，第二个是跳转的路径
 
-app.use(express.static(path.resolve(__dirname, 'public')))
+app.use(express.static(path.resolve(__dirname, 'public'))) //设置默认路径
+
 // 配置请求体
 app.use(express.urlencoded({ extended: true }))
 
 
 
 // 下面是服务器对客户端的响应
-app.use('/students', (req, res) => {
-    res.render('students', { students })
+app.use('/students', (req, res) => {  //路径
+    res.render('students', { students })  //需要渲染的对象名称及需要传递的参数， 用 {} 配置的形式传递。
 })
 
+
+// 添加学生
 app.post('/add-student', (req, res) => {
     console.log(req.body);
     let student = req.body
@@ -55,8 +39,16 @@ app.post('/add-student', (req, res) => {
 
     students.push(student)
 
+    // 将文件写入本地json文件中
+    fs.writeFile(path.resolve(__dirname, './data/students.json'), JSON.stringify(students)).then(
+        res.redirect('/students')
+
+    ).catch(err => {
+
+    })
+
+
     // 添加完数据后需要返回学生列表的路径，使用重定向
-    res.redirect('/students')
 
 })
 
@@ -64,9 +56,17 @@ app.get('/delete-student', (req, res) => {
     let id = + req.query.id
     students = students.filter(student => student.id !== id)
 
+    fs.writeFile(path.resolve(__dirname, './data/students.json'), JSON.stringify(students)).then(
+        res.redirect('/students')
+
+    ).catch(err => {
+
+    })
+
     res.redirect('/students')
 })
 
+// 更新学生
 app.get('/update-student', (req, res) => {
     let id = + req.query.id
     let student = students.find(student => student.id == id)
@@ -82,6 +82,13 @@ app.post('/update-student', (req, res) => {
     student.name = name
     student.age = age
     student.gender = gender
+
+    fs.writeFile(path.resolve(__dirname, './data/students.json'), JSON.stringify(students)).then(
+        res.redirect('/students')
+
+    ).catch(err => {
+
+    })
 
     res.redirect('/students')
 })

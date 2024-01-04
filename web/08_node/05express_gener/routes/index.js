de6d3318ae8325c71517e162120954352b3b0442
andydo365 <huconghuan@126.com>
2024-01-04 14:23:43 +0800

    express generator use and create little demo

--- web/08_node/05express_gener/routes/index.js
+++ web/08_node/05express_gener/routes/index.js
@@ -1,9 +1,34 @@
-var express = require('express');
+var express = require("express");
+const { formidable } = require("formidable");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
+router.get("/portrait", (req, res) => {
+  res.render("portrait");
+});
+
+router.post("/portrait", function (req, res, next) {
+  const form = formidable({
+    uploadDir: __dirname + "/../public/images", //一般路径是 ../public/images ，但是
+    keepExtensions: true,
+    multiples: true,
+  });
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    console.log(fields); // 存储input表单内容
+    console.log(files); //存储文件内容
+    let img_server_url = files.portrait.newFilename;
+    let url = "/imges/" + img_server_url;  //以后将数据存到数据库中
+
+    res.send(url);
+  });
+  // res.send("success");
+});
 module.exports = router;

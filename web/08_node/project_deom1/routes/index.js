6188a1b638076eac67c59ba12e6b8a324775b46c
andydo365 <huconghuan@126.com>
2024-01-10 15:13:34 +0800

    learn mongodb

--- web/08_node/project_deom1/routes/index.js
+++ web/08_node/project_deom1/routes/index.js
@@ -1,12 +1,27 @@
 var express = require("express");
 var router = express.Router();
+var fs = require("fs");
+let data = require(__dirname + "/../data/data.json");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index");
+  res.render("index", { data });
 });
 
 router.get("/add", function (req, res, next) {
-  res.render("index");
+  res.render("add");
+});
+router.post("/add", (req, res) => {
+  // console.log(req.body);
+  let newData = {
+    id: data.length + 1,
+    ...req.body,
+  };
+  data.push(newData);
+  fs.writeFile(__dirname + "/../data/data.json", JSON.stringify(data), (err) =>
+    console.log(err.message)
+  );
+  // res.send("提交成功");
+  res.redirect("/");
 });
 module.exports = router;

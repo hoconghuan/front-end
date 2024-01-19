630fd7a085a0ce4074e27cf234b65efe02098d49
andydo365 <huconghuan@126.com>
2024-01-19 09:42:48 +0800

    mult router finish

--- web/08_node/10_mongo/account_note_Plus/app.js
+++ web/08_node/10_mongo/account_note_Plus/app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/lg", authRouter);
+app.use("/user", authRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
